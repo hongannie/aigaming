d76b73c39eed03aaf27f44a803222e977282455a
nguyenub <70727247+nguyenub@users.noreply.github.com>
2021-04-16 19:25:15 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,9 @@
+In the text entry write up to 3 paragraphs explaining what you are going to work on and how you will organize your code/project
+If you work on a leetcode style project, mention all the problems you decided to work on
+If you work on a default project, mention which parts you will do and how you will organize your code overall
+If you have a custom project, explain what it is and what needs to be done
+
+
+AI project: using dynamic programming to solve a Markov decision process
+For those of you who would like to combine algorithms and artificial intelligence, a possible project is to model some game as a Markov decision process and solve it using dynamic programming. These are some of the fundamental concepts behind AI methods based on reinforcement learning.
 
